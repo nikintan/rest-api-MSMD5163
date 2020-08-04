fc78bc07d5ecc81ca0d65bd28bf7f0e8d95d79e2
nikintan <intanzura@yahoo.com>
2020-08-04 11:51:41 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.port || 8080;
+
+var router = express.Router();
+router.get('/',(req, res)=>{
+	res.json({ message: 'hooray! welcome to our API!'});
+});
+
+router.get('/hello',(req, res)=>{
+	res.json({ message: 'Hello World!'});
+});
+
+router.get('/goodbye/:name',(req, res)=>{
+	res.json({ message: 'Goodbye ' + req.params.name});
+});
+
+router.post('/sum',(req, res)=>{
+	var sum = req.body.NUM1 + req.body.NUM2;
+	res.json({message:'The sum is ' +sum});
+});
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('Magic happen on port ' + port);
